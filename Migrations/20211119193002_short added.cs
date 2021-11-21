bd8ca6c9094f10b9dd655e345ff3918bddae59f6
GrOeNFrUkT <patrik.svensson@bringles.se>
2021-11-21 15:10:47 +0100

    Add project files.

---
+++ Migrations/20211119193002_short added.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class shortadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Short",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Short",
+                table: "Products");
+        }
+    }
+}
